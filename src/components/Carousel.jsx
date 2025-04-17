7c81e625480d2b90be418a6f5c717d3b597ec2ac
joacodimaro97 <joacodimaro97@gmail.com>
2025-04-17 12:56:21 -0300

    add cambios en services

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -86,6 +86,38 @@ const Carousel = () => {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="relative overflow-hidden rounded-2xl shadow-xl p-2 md:p-6">
+        <div className="text-left mb-12">
+           <h2 className="text-3xl md:text-4xl font-bold text-[#146c2c]">Nuestros Servicios</h2>
+           <p className="text-lg py-10  text-[#4c4c4c]">
+               Desde 2015, recorremos rutas nacionales e internacionales cuidando cada detalle. Somos expertos en el traslado de animales (bovinos, porcinos, equinos) y cereales, garantizando seguridad, puntualidad y bienestar en cada kilómetro.
+             </p>
+           <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+             <div className="bg-[#f0fdf4] rounded-lg p-4 text-center shadow hover:shadow-md transition-all">
+               <span className="icon-[mdi--cow] text-4xl text-[#146c2c] mb-2 inline-block" />
+               <h3 className="font-bold text-[#146c2c]">Transporte Bovino</h3>
+               <p className="text-sm text-gray-600 mt-2">Especializado para el traslado seguro de ganado bovino.</p>
+             </div>
+             <div className="bg-[#f0fdf4] rounded-lg p-4 text-center shadow hover:shadow-md transition-all">
+               <span className="icon-[mdi--horse] text-4xl text-[#146c2c] mb-2 inline-block" />
+               <h3 className="font-bold text-[#146c2c]">Transporte Equino</h3>
+               <p className="text-sm text-gray-600 mt-2">Cuidado y confort para caballos durante el viaje.</p>
+             </div>
+             <div className="bg-[#f0fdf4] rounded-lg p-4 text-center shadow hover:shadow-md transition-all">
+               <span className="icon-[mdi--pig] text-4xl text-[#146c2c] mb-2 inline-block" />
+               <h3 className="font-bold text-[#146c2c]">Transporte Porcino</h3>
+               <p className="text-sm text-gray-600 mt-2">Condiciones higiénicas y controladas para porcinos.</p>
+             </div>
+             <div className="bg-[#f0fdf4] rounded-lg p-4 text-center shadow hover:shadow-md transition-all">
+               <span className="icon-[mdi--corn] text-4xl text-[#146c2c] mb-2 inline-block" />
+               <h3 className="font-bold text-[#146c2c]">Transporte de Cereal</h3>
+               <p className="text-sm text-gray-600 mt-2">Transporte eficiente y seguro para granos y cereales.</p>
+             </div>
+           </div>
+ 
+           <p className="mt-10 text-xl md:text-xl font-bold text-[#146c2c]">
+             Nuestro transporte brinda:
+           </p>
+         </div>
           <div className="w-full overflow-hidden">
             <div
               ref={sliderRef}
