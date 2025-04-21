94bcc944800d9b05b1e038c8e64a8206f5e6434b
joacodimaro97 <joacodimaro97@gmail.com>
2025-04-21 17:19:08 -0300

    add mensaje predeterminado en whatsapp

--- src/components/LinkWhatsapp.jsx
+++ src/components/LinkWhatsapp.jsx
@@ -4,7 +4,7 @@ import { MessageCircle } from "lucide-react"
 export default function LinkWhatsapp(){
     return(
         <a
-      href="https://wa.me/5492227627929"
+      href="https://wa.me/5492227627929?text=%C2%A1Hola!%20Visité%20su%20p%C3%A1gina%20web%20y%20me%20gustar%C3%ADa%20obtener%20m%C3%A1s%20informaci%C3%B3n."
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-2 md:bottom-10 right-2 z-50  transition-all duration-300"
